d0394b6ccb0026900516ebf5bd2e52ba5f48ad1a
diegofmo0802 <diegofmo0802@gmail.com>
2023-12-30 04:43:02 -0500

    Se añadieron comentarios a el archivo principal de ServerCore

--- ServerCore.d.ts
+++ ServerCore.d.ts
@@ -5,19 +5,24 @@
  * @license Apache-2.0
  */
 
+// Utilidades principales del modulo.
 import Debug from "./Debug/Debug.js";
 import Template from "./Template/Template.js";
 import ServerCore from "./Server/Server.js";
 
+// Utilidades en desarrollo y sin añadir (Beta).
 import JsonWT from "./JsonWT/JsonWT.js";
 import Mail from "./Mail/Mail.js";
 
+// Para exportar las utilidades en desarrollo y sin añadir.
 const Beta = {
 	JsonWT, Mail
 };
 
+// Exportación de utilidades.
 export {
 	Debug, Template, ServerCore, Beta
 };
 
+// Exportación por defecto de ServerCore.
 export default ServerCore;
