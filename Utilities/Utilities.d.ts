c5c5c50c2b5b471d4ca738e279f3346eedf1194b
diegofmo0802 <diegofmo0802@gmail.com>
2024-03-03 16:48:52 -0500

    Correxion de errores con SendFolder

--- Utilities/Utilities.d.ts
+++ Utilities/Utilities.d.ts
@@ -4,6 +4,12 @@ declare class Utilities {
     public static Path: {
         ModuleDir: string,
         ModuleMain: string
+        /**
+         * Limpia una ruta.
+         * - Transforma las `\` en `/`
+         * @param Path La ruta que desea limpiar.
+         */
+        Normalize: (Path: string) => string
     }
 };
 
--- Utilities/Utilities.js
+++ Utilities/Utilities.js
@@ -7,7 +7,11 @@ const ModuleDir = PATH.dirname(PATH.dirname(URL.fileURLToPath(import.meta.url)))
 class Utilities {
     static Path = {
         ModuleDir: ModuleDir,
-        ModuleMain: `${ModuleDir}\\ServerCore.js`
+        ModuleMain: `${ModuleDir}\\ServerCore.js`,
+        Normalize: (Path) => {
+            Path = Path.replace(/[\\/]/gi, PATH.sep);
+            return Path;
+        }
     }
 };
 
