5b329b62df4665b05bae16fd3209ae2fdcae2f44
diegofmo0802 <diegofmo0802@gmail.com>
2025-05-18 06:45:07 +0000

    docs: JSDocs changed to English in Config.ts

--- src/Utilities/Path.ts
+++ src/Utilities/Path.ts
@@ -5,23 +5,22 @@ export class Path {
     public static readonly moduleDir: string = PATH.dirname(PATH.dirname(PATH.dirname(URL.fileURLToPath(import.meta.url))));
     public static readonly moduleMain: string = PATH.join(this.moduleDir, 'build/ServerCore.js');
     /**
-     * clean a path.
-     * - replaces all `\` with `/`
-     * @param path the path to clean
-     * @returns the cleaned path
+     * Clean a path by replacing all backslashes with the system-specific separator.
+     * @param path - The path to clean
+     * @returns The cleaned path
      */
     public static normalize(path: string): string {
         path = path.replace(/[\\/]/gi, PATH.sep);
         return path;
     }
     /**
-     * convert a relative path to a full path.
-     * @param path the relative path.
-     * @returns the full path.
+     * Convert a relative path to an absolute one, relative to the module root.
+     * @param path - The relative path
+     * @returns The absolute path
      */
     public static relative(path: string): string {
         path = this.normalize(path);
-        path =  PATH.join(this.moduleDir, path);
+        path = PATH.join(this.moduleDir, path);
         return PATH.normalize(path);
     }
 }
