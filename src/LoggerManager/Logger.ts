76b14e42ed00a00a604ffb697936f94efc06b6c9
diegofmo0802 <diegofmo0802@gmail.com>
2025-06-03 18:12:42 +0000

    refactor: Logger now implements the Debug.customLog method

--- src/LoggerManager/Logger.ts
+++ src/LoggerManager/Logger.ts
@@ -5,22 +5,31 @@ export class Logger {
     private prefix: string;
     private format: string;
     private debug: Debug;
+    public show: boolean = true;
+    public save: boolean = true;
     constructor(options: Logger.Options) {
         const { prefix, format = '&R&C7', debug } = options;
         this.prefix = prefix;
         this.format = format;
         this.debug = debug ?? Debug.getInstance(ConsoleUI.cleanFormat(prefix));
     }
-    public get save(): boolean { return this.debug.save; }
-    public set save(value: boolean) { this.debug.save = value; }
-    public get show(): boolean { return this.debug.show; }
-    public set show(value: boolean) { this.debug.show = value; }
-    public log (...data: any[]) { this.debug.log(`&C2[${this.prefix}&C2]:${this.format}`, ...data); }
-    public warn (...data: any[]) { this.debug.warn(`&C3[${this.prefix}&C3]:${this.format}`, ...data); }
-    public info (...data: any[]) { this.debug.info(`&C6[${this.prefix}&C6]:${this.format}`, ...data); }
-    public error (...data: any[]) { this.debug.error(`&C1[${this.prefix}&C1]:${this.format}`, ...data); }
+    public log (...data: any[]) { this.debug.customLog(data, this.getLogOptions('log')); }
+    public warn (...data: any[]) { this.debug.customLog(data, this.getLogOptions('warn')); }
+    public info (...data: any[]) { this.debug.customLog(data, this.getLogOptions('info')); }
+    public error (...data: any[]) { this.debug.customLog(data, this.getLogOptions('error')); }
+    private getLogOptions(level: Logger.level): Debug.LogOptions {
+        level = level.toLowerCase() as Logger.level;
+        let prefix = `&C2[LOG] [${this.prefix}&C2]${this.format}`;
+        switch (level) {
+            case 'warn': prefix = `&C3[WAN] [${this.prefix}&C3]${this.format}`; break;
+            case 'info': prefix = `&C6[INF] [${this.prefix}&C6]${this.format}`; break;
+            case 'error': prefix = `&C1[ERR] [${this.prefix}&C1]${this.format}`; break;
+        }
+        return { prefix, save: this.save, show: this.show, }
+    }
 }
 export namespace Logger {
+    export type level = 'log' | 'warn' | 'info' | 'error';
     export interface Options {
         prefix: string;
         format?: string;
