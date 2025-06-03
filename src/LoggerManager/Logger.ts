d41325e3bcff18af8039aaffb8c0422e6142f8d2
diegofmo0802 <diegofmo0802@gmail.com>
2025-06-03 18:49:07 +0000

    refactor: added more methods to instantiate Logger

--- src/LoggerManager/Logger.ts
+++ src/LoggerManager/Logger.ts
@@ -2,6 +2,12 @@ import ConsoleUI from "../ConsoleUI.js";
 import Debug from "../Debug.js";
 
 export class Logger {
+    private static readonly LEVEL_MAP: Logger.LevelMap = {
+        log: `&C2[LOG]`,
+        warn: `&C3[WRN]`,
+        info: `&C6[INF]`,
+        error: `&C1[ERR]`,
+    }
     private prefix: string;
     private format: string;
     private debug: Debug;
@@ -11,29 +17,33 @@ export class Logger {
         const { prefix, format = '&R&C7', debug } = options;
         this.prefix = prefix;
         this.format = format;
-        this.debug = debug ?? Debug.getInstance(ConsoleUI.cleanFormat(prefix));
+        if (debug instanceof Debug) this.debug = debug;
+        else if (typeof debug === 'string') this.debug = Debug.getInstance(debug);
+        else if (debug) this.debug = Debug.getInstance(debug.id, debug);
+        else this.debug = Debug.getInstance(ConsoleUI.cleanFormat(prefix));
     }
     public log (...data: any[]) { this.debug.customLog(data, this.getLogOptions('log')); }
     public warn (...data: any[]) { this.debug.customLog(data, this.getLogOptions('warn')); }
     public info (...data: any[]) { this.debug.customLog(data, this.getLogOptions('info')); }
     public error (...data: any[]) { this.debug.customLog(data, this.getLogOptions('error')); }
     private getLogOptions(level: Logger.level): Debug.LogOptions {
-        level = level.toLowerCase() as Logger.level;
-        let prefix = `&C2[LOG] [${this.prefix}&C2]${this.format}`;
-        switch (level) {
-            case 'warn': prefix = `&C3[WAN] [${this.prefix}&C3]${this.format}`; break;
-            case 'info': prefix = `&C6[INF] [${this.prefix}&C6]${this.format}`; break;
-            case 'error': prefix = `&C1[ERR] [${this.prefix}&C1]${this.format}`; break;
-        }
+        const prefix = `${Logger.LEVEL_MAP[level]} [${this.prefix}]${this.format}`
         return { prefix, save: this.save, show: this.show, }
     }
 }
 export namespace Logger {
     export type level = 'log' | 'warn' | 'info' | 'error';
+    export type LevelMap = Record<level, string>;
+    export interface NewDebugOptions {
+        id: string;
+        path?: string;
+        show?: boolean;
+        save?: boolean;
+    }
     export interface Options {
         prefix: string;
         format?: string;
-        debug?: Debug;
+        debug?: string | Debug | NewDebugOptions;
     }
 }
 export default Logger;
